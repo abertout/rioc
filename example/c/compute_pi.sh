973059f4f83a2debd05cc57389858858fe101365
Cedric Doucet <cedric.doucet@inria.fr>
2015-05-27 15:19:20 +0200

    Load mpi module and solve a deprecation issue

--- example/c/compute_pi.sh
+++ example/c/compute_pi.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-/cm/shared/apps/openmpi/1.8.4/bin/mpirun \
-    -machinefile $OAR_FILE_NODES \
-    -mca pls_rsh_agent "oarsh" \
-    -np 4 \
-    compute_pi
+module load mpi
+mpirun -machinefile $OAR_FILE_NODES \
+       -mca plm_rsh_agent "oarsh" \
+       -np 4 \
+       compute_pi
+unload module mpi
